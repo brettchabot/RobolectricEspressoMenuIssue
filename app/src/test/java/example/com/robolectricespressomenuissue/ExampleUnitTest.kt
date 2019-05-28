69e2c96f0ab7bfc85ca5e60fb13f648169568cfa
Brett Chabot <brettchabot@google.com>
2019-05-28 12:08:48 -0700

    Update to 4.3-beta-1 and make test pass

--- app/src/test/java/example/com/robolectricespressomenuissue/ExampleUnitTest.kt
+++ app/src/test/java/example/com/robolectricespressomenuissue/ExampleUnitTest.kt
@@ -5,11 +5,15 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.rule.ActivityTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.Config.ALL_SDKS
+import org.robolectric.annotation.LooperMode
+import org.robolectric.annotation.TextLayoutMode
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -17,9 +21,12 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@TextLayoutMode(TextLayoutMode.Mode.REALISTIC)
+@LooperMode(LooperMode.Mode.PAUSED)
+@Config(sdk = [ALL_SDKS])
 class ExampleUnitTest {
     @get:Rule
-    val activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java, false, true)
+    val activityScenarioRule = ActivityScenarioRule<MainActivity>(MainActivity::class.java)
 
     @Test
     fun testOpenMenuItem() {
